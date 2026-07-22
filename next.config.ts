6fad8143cefc81290d0f785a14cd97c7f86d5cdb
alsoSydneyy <Sydney00769@gmail.com>
2026-07-22 16:01:24 +0700

    migratio

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fastestlap.store',
+      },
     ],
   },
 };
